0bf1366e094905a655aa60d5fd3c7036e4f786ee
moise10r <moiserushanika2006@gmail.com>
2021-06-29 20:23:08 +0200

    Add show more logic on team members.

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ const close = document.querySelector('.close');
 const mainNavbar = document.querySelector('.main-navbar');
 const memberlist = document.querySelector('.team-members');
 const navLink = document.querySelectorAll('.nav-list .nav-link a');
-
 Array.from(navLink).forEach((item) => {
   item.addEventListener('click', () => {
     mainNavbar.classList.remove('open');
@@ -47,4 +46,26 @@ data.forEach((member) => {
     <p class="member-detail">${member.work}</p>
   </div>
 </li>`;
+});
+
+const memberList = document.querySelectorAll('.member');
+const btn = document.querySelector('.see-more .btn');
+let state = true;
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (state) {
+    Array.from(memberList).forEach((member) => {
+      member.classList.add('show');
+      btn.innerHTML = '<span>Hide</span><span><i class="fa fa-chevron-up"></i></span>';
+    });
+    state = false;
+  } else {
+    state = false;
+    Array.from(memberList).forEach((member) => {
+      member.classList.remove('show');
+      btn.innerHTML = '<span>More</span><span><i class="fa fa-chevron-down"></i></span>';
+    });
+    state = true;
+  }
 });
