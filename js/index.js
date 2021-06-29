c75148d2e1c37983cc7540053e0886205d31094d
moise10r <moiserushanika2006@gmail.com>
2021-06-29 18:35:15 +0200

    Render members's list dynamically.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+import data from './data.js';
+
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.close');
 const mainNavbar = document.querySelector('.main-navbar');
+const memberlist = document.querySelector('.team-members');
 
 menu.addEventListener('click', () => {
   mainNavbar.classList.add('open');
@@ -8,4 +11,22 @@ menu.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   mainNavbar.classList.remove('open');
+});
+
+data.forEach((member) => {
+  memberlist.innerHTML += `<li class="member">
+  <div class="member-profile">
+    <div class="img-profile">
+    <img class="img_1" src=${member.profile} alt="" />
+    </div>
+    <img class="img_2" src="/Images/grid.jpg" alt="" />
+  </div>
+  <div class="about-member">
+    <h2 class="name">${member.memberName}</h2>
+    <i
+      >${member.about}</i
+    >
+    <p class="member-detail">${member.work}</p>
+  </div>
+</li>`;
 });
